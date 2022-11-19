915f0f3c6be3099115a99a67b33e62c1afa2ab1b
Bin-08-01 <minhtri192035@gmail.com>
2022-11-19 07:19:18 +0700

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,22 @@
 import React from "react";
-import {
-  Route,
-  Routes
-} from "react-router-dom";
-import Login from "./Component/Login"
-import Register from "./Component/Register";
-import Home from "./Component/Home";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import Login from "./Component/login/Login"
+import Register from "./Component/register/Register";
+import Home from "./Component/home/Home";
 import './App.css';
+import Profile from "./Component/profile/Profile";
 function App() {
-
-
   return (
-    <>
-      <div className=' bg-emerald-800 bg-opacity-60 w-full h-screen flex justify-center items-center' >
+    <Router>
+      <div id='main-component' className='bg-neutral-400 bg-opacity-60 w-full h-screen flex justify-center items-center'>
         <Routes>
-          <Route path="/" element={<Login />}></Route>
-          <Route path="/register" element={<Register />}></Route>
-          <Route path="/home" element={<Home></Home>}></Route>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/register" element={<Register/>}/>
+          <Route path="/" element={<Home/>}/>
+            <Route path="/profile" element={<Profile/>}/>
         </Routes>
       </div>
-    </>
+    </Router>
   );
 }
 
