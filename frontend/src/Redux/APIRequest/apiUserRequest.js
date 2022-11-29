190f58497713426088241db191d7f6a4dc56010d
Bin-08-01 <minhtri192035@gmail.com>
2022-11-30 00:33:57 +0700

    Change path axios

---
+++ frontend/src/Redux/APIRequest/apiUserRequest.js
@@ -0,0 +1,17 @@
+import {instance} from "../../createInstance";
+
+export const getInfo = async (idUser) => {
+    try{
+        return instance.get(`/v1/user/info/${idUser}`);
+    }catch(err){
+        console.log(err);
+    }
+}
+
+export const changePassword = async (data, dispatch, axiosJWT) => {
+    return await axiosJWT.post("/v1/user/change/password", data);
+}
+
+export const changeAvatar = async (data, dispatch, axiosJWT) => {
+    return await axiosJWT.post("/v1/user/change/avatar", data);
+}
