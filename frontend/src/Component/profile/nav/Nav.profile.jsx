a418012096d9c12e3198c2f2d482d00d3d506acd
Bin-08-01 <minhtri192035@gmail.com>
2022-12-03 20:52:17 +0700

    Remove Storage

--- frontend/src/Component/profile/nav/Nav.profile.jsx
+++ frontend/src/Component/profile/nav/Nav.profile.jsx
@@ -7,7 +7,7 @@ import {IoPeopleOutline} from "react-icons/io5";
 function NavProfile() {
     return (
         <nav className="h-screen flex fixed flex-col border-solid border-0 border-r-2 border-r-gray-200 z-50 top-0 left-12 w-60">
-            <div className={"w-full text-center border-solid border-0 border-b-2 border-gray-200 py-3 px-2"}>
+            <div className={"w-full text-center border-solid border-0 border-b-2 border-gray-200 py-3 px-2 h-14"}>
                 <h1 className={""}>Profile</h1>
             </div>
             <div className={"flex flex-col ml-8 mt-4"}>
@@ -15,7 +15,6 @@ function NavProfile() {
                 <Link to={"detail"} className={"text-gray-500 my-3"}><MdArticle/> My details</Link>
                 <Link to={"password"} className={"text-gray-500 my-3"}><CgPassword/>  Password</Link>
                 <Link to={"follow"} className={"text-gray-500 my-3"}><IoPeopleOutline/> Follow</Link>
-                <Link to={"storage"} className={"text-gray-500 my-3"}><MdStorage/> Storage</Link>
             </div>
         </nav>
     )
